d4ddf6b2acf99b0c58d9a15e8bff0dabd4d6018e
Fabian <sabljak_fabian@web.de>
2019-01-15 19:49:55 +0100

    -

--- transfer_learning.py
+++ transfer_learning.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torch.autograd import Variable
 from torchvision import datasets, models, transforms
@@ -9,15 +10,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import shutil
 from PIL import ImageFile
+from sklearn.metrics import confusion_matrix
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-class Config:
+class Config(datasets.ImageFolder):
     def __init__(self, data_dir):
         # Data augmentation and normalization for training
         # Just normalization for validation
+        """
         self.data_transforms = {
             'train': transforms.Compose([
                 # transforms.RandomResizedCrop(224),
@@ -33,30 +36,58 @@ class Config:
                 # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ]),
         }
+        """
+        self.data_transform = transforms.Compose([
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+
         self.data_dir = data_dir
-        self.ckpt_dir = os.path.join(data_dir, 'checkpoints')
+        self.ckpt_dir = os.path.join(os.path.split(data_dir)[:-1][0], 'checkpoints')
+        self.output_dir = os.path.join(os.path.split(data_dir)[:-1][0], 'output')
         # Create a dictionary that contains the information of the images in both the training and validation set
+        """
         self.image_datasets = {x: datasets.ImageFolder(os.path.join(self.data_dir, x), self.data_transforms[x])
                                for x in ['train', 'val']}
+        """
+        self.image_dataset = datasets.ImageFolder(self.data_dir, self.data_transform)
+        """
         # Create a dictionary that contains the data loader
-        self.dataloader = {x: torch.utils.data.DataLoader(self.image_datasets[x], batch_size=32, shuffle=True)
+        self.dataloader= {x: torch.utils.data.DataLoader(self.image_datasets[x], batch_size=4, shuffle=True)
                            for x in ['train', 'val']}
+        """
         # Create a dictionary that contains the size of each dataset (training and validation)
+        """
         self.dataset_sizes = {x: len(self.image_datasets[x]) for x in ['train', 'val']}
+        """
+        self.dataset_size = len(self.image_dataset)
         # Get the class names
-        self.class_names = self.image_datasets['train'].classes
+        self.class_names = self.image_dataset.classes
         # Print out the results
         print("Class Names: {}".format(self.class_names))
-        print("{} batches in the training set".format(len(self.dataloader['train'])))
-        print("{} batches in the test set".format(len(self.dataloader['val'])))
-        print("{} training images".format(self.dataset_sizes['train']))
-        print("{} testing images".format(self.dataset_sizes['val']))
+        # print("{} batches in the training set".format(len(self.dataloader['train'])))
+        # print("{} batches in the test set".format(len(self.dataloader['val'])))
+        print("{} training images".format(self.dataset_size))
+        # print("{} testing images".format(self.dataset_sizes['val']))
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, idx):
+        path, label = self.image_dataset.imgs[idx]
+        sample = self.image_dataset.loader(path)
+        sample = self.data_transform(sample)
+        # return super(Config, self).__getitem__(idx), self.imgs[idx]
+        return sample, label, path
 
 
 class Model:
-    def __init__(self, config):
-        self.dataloader = config.dataloader
-        self.config = config
+    def __init__(self, _train_loader, _val_loader, epochs):
+        self.train_loader = _train_loader
+        self.val_loader = _val_loader
+        self.num_epochs = epochs
+
         # Load the ResNet
         self.model = torchvision.models.resnet18(pretrained=True)
 
@@ -68,41 +99,16 @@ class Model:
         num_ftrs = self.model.fc.in_features
         # Reconstruct the last layer (output layer) to have seven classes
         self.model.fc = nn.Linear(num_ftrs, 7)
-        self.model.class_to_idx = self.dataloader['train'].dataset.class_to_idx
+        # get class indices
+        self.model.class_to_idx = self.val_loader.dataset.image_dataset.class_to_idx
 
         if torch.cuda.is_available():
             self.model = self.model.cuda()
 
-        """
-        # Understand what's happening
-        iteration = 0
-        correct = 0
-        for inputs, labels in self.dataloader['train']:
-            if iteration == 1:
-                break
-            inputs = Variable(inputs)
-            labels = Variable(labels)
-            if torch.cuda.is_available():
-                inputs = inputs.cuda()
-                labels = labels.cuda()
-            print("For one iteration, this is what happens:")
-            print("Input Shape:", inputs.shape)
-            print("Labels Shape:", labels.shape)
-            print("Labels are: {}".format(labels))
-            output = self.model(inputs)
-            print("Output Tensor:", output)
-            print("Outputs Shape", output.shape)
-            _, predicted = torch.max(output, 1)
-            print("Predicted:", predicted)
-            print("Predicted Shape", predicted.shape)
-            correct += (predicted == labels).sum()
-            print("Correct Predictions:", correct)
-            iteration += 1
-        """
-
         self.criterion = nn.CrossEntropyLoss()
         # self.optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
         self.optimizer = optim.Adam(self.model.fc.parameters(), lr=0.001)
+
         # Decay LR by a factor of 0.1 every 7 epochs
         self.exp_lr_scheduler = lr_scheduler.StepLR(self.optimizer, step_size=7, gamma=0.1)
 
@@ -110,16 +116,20 @@ class Model:
         return np.exp(x) / np.sum(np.exp(x))
 
     def train(self):
-        num_epochs = 2
         best_acc = 0.0
-        df = pd.DataFrame()
-        for epoch in range(num_epochs):
+        # image_id = []
+        # df = pd.DataFrame()
+        for epoch in range(self.num_epochs):
             self.exp_lr_scheduler.step()
             # Reset the correct to 0 after passing through all the dataset
             correct = 0
-            for images, labels in self.dataloader['train']:
+            for images, labels, _ in self.train_loader:
                 images = Variable(images)
                 labels = Variable(labels)
+                """
+                for i in range(len(path)):
+                    image_id.append(os.path.splitext(os.path.split(path[i])[-1])[0])
+                """
                 if torch.cuda.is_available():
                     images = images.cuda()
                     labels = labels.cuda()
@@ -131,26 +141,34 @@ class Model:
                 self.optimizer.step()
                 _, predicted = torch.max(outputs, 1)
                 correct += (predicted == labels).sum()
+                """
                 # save to DataFrame
-                df.append(pd.Series(np.apply_along_axis(self.softmax, 1, outputs.cpu().detach().numpy())[0]),
-                               ignore_index=True)
+                df = df.append(
+                    pd.DataFrame(data=np.apply_along_axis(self.softmax, 1, outputs.cpu().detach().numpy())),
+                    ignore_index=True)
+                """
 
-            train_acc = 100 * correct.item() / self.config.dataset_sizes['train']
+            train_acc = 100 * correct.item() / len(self.train_loader.dataset)
             print('Epoch [{}/{}], Loss: {:.4f}, Train Accuracy: {}%'
-                  .format(epoch + 1, num_epochs, loss.item(), train_acc))
+                  .format(epoch + 1, self.num_epochs, loss.item(), train_acc))
             # save model for every epoch
-            print('[*] Saving model to {}'.format(self.config.ckpt_dir))
+            print('[*] Saving model to {}'.format(self.train_loader.dataset.ckpt_dir))
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': self.model.state_dict(),
                 'optimizer_state_dict': self.optimizer.state_dict(),
                 'class_to_idx': self.model.class_to_idx,
                 'loss': loss,
-            }, os.path.join(self.config.ckpt_dir, 'ckpt.pth.tar'))
-            # change idx to class names
+            }, os.path.join(self.train_loader.dataset.ckpt_dir, 'ckpt.pth.tar'))
+            """
+            # change idx to class names, rename df, add image_ids, arrange correctly and sort by im_ids
             idx_to_class = {val: key for key, val in self.model.class_to_idx.items()}
             df_classes = df.rename(idx_to_class, axis=1)
-            df_classes.to_csv(os.path.join(self.config.data_dir, 'output', 'outputs.csv'))
+            df_classes['image'] = image_id
+            df_classes = df_classes[['image', 'mel', 'nv', 'bcc', 'akiec', 'bkl', 'df', 'vasc']]
+            df_classes = df_classes.sort_values(by='image')
+            df_classes.to_csv(os.path.join(self.config.data_dir, 'output', 'outputs.csv'), index=False)
+            """
             # distinguish best model and save separately
             if train_acc > best_acc:
                 print('[*] ==== Best Acc Achieved ====')
@@ -160,26 +178,50 @@ class Model:
                     'optimizer_state_dict': self.optimizer.state_dict(),
                     'class_to_idx': self.model.class_to_idx,
                     'loss': loss,
-                }, os.path.join(self.config.ckpt_dir, 'best_model.pth.tar'))
-                df_classes.to_csv(os.path.join(self.config.data_dir, 'output', 'best_outputs.csv'))
+                }, os.path.join(self.train_loader.dataset.ckpt_dir, 'best_model.pth.tar'))
+                # df_classes.to_csv(os.path.join(self.config.data_dir, 'output', 'best_outputs.csv'), index=False)
 
     def test(self):
         self.model.eval()
         with torch.no_grad():
             correct = 0
             total = 0
-            for (images, labels) in self.dataloader['val']:
+            image_id = []
+            df = pd.DataFrame()
+            for images, labels, path in self.val_loader:
                 images = Variable(images)
                 labels = Variable(labels)
+                for i in range(len(path)):
+                    image_id.append(os.path.splitext(os.path.split(path[i])[-1])[0])
                 if torch.cuda.is_available():
                     images = images.cuda()
                     labels = labels.cuda()
 
                 outputs = self.model(images)
+                loss = self.criterion(outputs, labels)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+                # save to DataFrame
+                df = df.append(
+                    pd.DataFrame(data=np.apply_along_axis(self.softmax, 1, outputs.cpu().detach().numpy())),
+                    ignore_index=True)
 
+            # change idx to class names, rename df, add image_ids, arrange correctly and sort by im_ids
+            idx_to_class = {val: key for key, val in self.model.class_to_idx.items()}
+            df_classes = df.rename(idx_to_class, axis=1)
+            df_classes['image'] = image_id
+            df_classes = df_classes[['image', 'mel', 'nv', 'bcc', 'akiec', 'bkl', 'df', 'vasc']]
+            df_classes = df_classes.sort_values(by='image')
+            df_classes.to_csv(os.path.join(self.train_loader.dataset.output_dir, 'prob_table.csv'), index=False)
+
+            # Calculate metrics
+            accuracy = np.mean(np.equal(predicted, labels))
+            conf_mat = confusion_matrix(labels, predicted)
+            sensitivity = conf_mat.diagonal() / conf_mat.sum(axis=1)
+
+            # Print metrics
+            print("Test Accuracy", accuracy, "Test Sensitivity", np.mean(sensitivity), "Test loss", np.mean(loss))
             print('Test Accuracy: {:.3f} %'.format(100 * correct / total))
 
     def show_img(self):
@@ -188,7 +230,7 @@ class Model:
         shown_batch = 0
         index = 0
         with torch.no_grad():
-            for (images, labels) in self.dataloader['val']:
+            for images, labels, path in self.val_loader:
                 if shown_batch == 1:
                     break
                 shown_batch += 1
@@ -205,7 +247,7 @@ class Model:
                     index += 1
                     ax = plt.subplot(2, 2, index)
                     ax.axis('off')
-                    ax.set_title('Predicted Label: {}'.format(self.config.class_names[preds[i]]))
+                    ax.set_title('Predicted Label: {}'.format(self.val_loader.dataset.class_names[preds[i]]))
                     # Get the tensor of the image, and put it to cpu
                     input_img = images.cpu().data[i]
                     # If we have a tensor of shape (2,3,4) --> it becomes (3,4,2)
@@ -219,7 +261,10 @@ class Model:
 
 
 if __name__ == '__main__':
-    c = Config('data/lesion_data_multiclass')
-    m = Model(c)
+    train_dataset = Config('data/lesion_data_multiclass/train')
+    train_loader = DataLoader(train_dataset, batch_size=500, shuffle=True)
+    val_dataset = Config('data/lesion_data_multiclass/val')
+    val_loader = DataLoader(val_dataset, batch_size=500, shuffle=True)
+    m = Model(train_loader, val_loader, epochs=1)
     m.train()
     m.test()
